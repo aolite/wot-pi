ec53d805f22bf00c0892c15986929d04bdd8e9ca
aolite <aitorcorchero@gmail.com>
2020-03-31 01:00:08 +0200

    coap server and client

---
+++ src/utils/utils.ts
@@ -0,0 +1,22 @@
+import resourcesJson from '../resources/resources.json'; 
+
+const addDevice = (id, name, description, sensors= {}, actuators = {}) => {
+    if (!resourcesJson.hasOwnProperty('things')){
+        resourcesJson['things'] = {};
+    }
+
+    resourcesJson['things'][id] = {
+        'name': description, 
+        'sensors': sensors,
+        'actuators': actuators
+    }
+};
+
+const randomInt = (low, high) => {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+export {
+    addDevice, 
+    randomInt
+};
